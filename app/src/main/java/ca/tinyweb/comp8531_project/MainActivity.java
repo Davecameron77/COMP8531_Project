7fde7c5e016765d5e0f0adb4356ac536e4ac97da
Dave Cameron <dave@tinyweb.ca>
2022-06-12 20:53:35 -0700

    Init Baseline

--- app/src/main/java/ca/tinyweb/comp8531_project/MainActivity.java
+++ app/src/main/java/ca/tinyweb/comp8531_project/MainActivity.java
@@ -1,54 +1,83 @@
 package ca.tinyweb.comp8531_project;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.renderscript.Allocation;
 import android.renderscript.RenderScript;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.android.rs.hellocompute.ScriptC_mono;
 
-import java.util.concurrent.Executor;
-import java.util.concurrent.ThreadPoolExecutor;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private Bitmap mBitmapIn;
     private Bitmap mBitmapOut;
+    private final int ITERATIONS = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mBitmapIn = loadBitmap(R.drawable.data);
-        mBitmapOut = Bitmap.createBitmap(mBitmapIn.getWidth(), mBitmapIn.getHeight(),
-                mBitmapIn.getConfig());
-
         ImageView in = findViewById(R.id.displayin);
-        in.setImageBitmap(mBitmapIn);
-
         ImageView out = findViewById(R.id.displayout);
-        out.setImageBitmap(mBitmapOut);
 
-        createScript();
+        long startTimeInMills = System.currentTimeMillis();
+
+        for (int i=0; i<=ITERATIONS; i++) {
+            int rand = new Random().nextInt((4 - 1) + 1) + 1;
+
+            switch (rand) {
+                case 1:
+                    mBitmapIn = loadBitmap(R.drawable.data);
+                    break;
+                case 2:
+                    mBitmapIn = loadBitmap(R.drawable.data1);
+                    break;
+                case 3:
+                    mBitmapIn = loadBitmap(R.drawable.data2);
+                    break;
+                case 4:
+                    mBitmapIn = loadBitmap(R.drawable.data3);
+                    break;
+            }
+            mBitmapIn = loadBitmap(R.drawable.data);
+            mBitmapOut = Bitmap.createBitmap(mBitmapIn.getWidth(), mBitmapIn.getHeight(), mBitmapIn.getConfig());
+
+            in.setImageBitmap(mBitmapIn);
+            createScript();
+            out.setImageBitmap(mBitmapOut);
+        }
+
+        long endTimeInMills = System.currentTimeMillis();
+        long executionTime = endTimeInMills - startTimeInMills;
+        Toast.makeText(this, "Execution took " + executionTime + " ms for " + ITERATIONS + " iterations", Toast.LENGTH_LONG).show();
+
     }
 
+    /**
+     * Renderscript execution
+     */
     private void createScript() {
         RenderScript mRS = RenderScript.create(this);
-        Allocation mInAllocation = Allocation.createFromBitmap(mRS,
-                mBitmapIn,
-                Allocation.MipmapControl.MIPMAP_NONE,
-                Allocation.USAGE_SCRIPT);
+        Allocation mInAllocation = Allocation.createFromBitmap(mRS, mBitmapIn, Allocation.MipmapControl.MIPMAP_NONE, Allocation.USAGE_SCRIPT);
         Allocation mOutAllocation = Allocation.createTyped(mRS, mInAllocation.getType());
         ScriptC_mono mScript = new ScriptC_mono(mRS);
         mScript.forEach_root(mInAllocation, mOutAllocation);
         mOutAllocation.copyTo(mBitmapOut);
     }
 
+    /**
+     * Helper method to fetch a bitmap from Drawable
+     * @param resource The resource ID to fetch
+     * @return The Bitmap that was loaded
+     */
     private Bitmap loadBitmap(int resource) {
         final BitmapFactory.Options options = new BitmapFactory.Options();
         options.inPreferredConfig = Bitmap.Config.ARGB_8888;
--- app/src/main/java/ca/tinyweb/comp8531_project/mono.rs
+++ app/src/main/java/ca/tinyweb/comp8531_project/mono.rs
@@ -1,11 +1,10 @@
 #pragma version(1)
-#pragma rs java_package_name(com.example.android.rs.hellocompute)
+#pragma rs java_package_name(ca.tinyweb.COMP8531_Project.rs.hellocompute)
 
 const static float3 gMonoMult = {0.299f, 0.587f, 0.114f};
 
 void root(const uchar4 *v_in, uchar4 *v_out) {
     float4 f4 = rsUnpackColor8888(*v_in);
-
     float3 mono = dot(f4.rgb, gMonoMult);
     *v_out = rsPackColorTo8888(mono);
 }
